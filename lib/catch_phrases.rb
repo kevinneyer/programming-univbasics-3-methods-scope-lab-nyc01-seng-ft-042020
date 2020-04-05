62851e0f456f08d06dea24c195448728aefe7b00
Kevin Neyer <kevinneyer@gmail.com>
2020-04-05 18:52:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,8 @@ def toadstool
    status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  phrase_2 = "It's Dangerous To Go Alone! Take This."
+  puts phrase_2
+end
